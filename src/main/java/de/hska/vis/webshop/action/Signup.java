b81cb018cd1f1e8f16b73045b63ac2e52ce6ade9
JakobErnst <jakob_03@web.de>
2015-12-07 13:11:38 +0100

    Implemented validation for textfields on the Signup page

--- src/main/java/de/hska/vis/webshop/action/Signup.java
+++ src/main/java/de/hska/vis/webshop/action/Signup.java
@@ -3,9 +3,12 @@ package de.hska.vis.webshop.action;
 import com.opensymphony.xwork2.ActionSupport;
 import de.hska.vis.webshop.helper.HibernateUtil;
 import de.hska.vis.webshop.model.User;
+import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
+
+import java.util.List;
 
 /**
  * Created by Christian on 25.11.2015.
@@ -47,7 +50,90 @@ public class Signup extends ActionSupport {
 
     }
 
+    /**
+     * Validates the input in the textfields on the signup page.
+     */
+    @Override
     public void validate(){
+
+        checkNullInput();
+
+        checkPassword();
+
+        checkEmail();
+
+    }
+
+    /**
+     * Checks if the E-mail field is empty or if the email is already given to another Account.
+     */
+    private void checkEmail(){
+
+        if(userBean.getEmail() != null){
+            addFieldError("userBean.getEmail","Email is Required");
+        }else {
+
+            Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+
+            session.beginTransaction();
+
+            String sql = "from User as u where u.email=:mail";
+            Query query = session.createQuery(sql);
+            query.setParameter("mail", userBean.getEmail());
+            List<User> list = query.list();
+
+            session.close();
+
+            if (list.size() > 0) {
+                addFieldError("userBean.getEmail", "E-mail address already exists");
+            }
+        }
+    }
+
+    /**
+     * Checks if the Password is empty or shorter than eight characters.
+     */
+    private void checkPassword(){
+
+        if(userBean.getPassword() == null) {
+            addFieldError("userBean.getPassword", "Password is Required");
+        }else if(userBean.getPassword().length() < 8) {
+            addFieldError("userBean.getPassword", "Password has to be at least eight characters long");
+        }
+    }
+
+    /**
+     * checks if fields but the password and email are empty.
+     */
+    private void checkNullInput(){
+
+        if(userBean.getCity() != null){
+            addFieldError("userBean.getCity","City is Required");
+        }
+
+        if(userBean.getCountry() != null){
+            addFieldError("userBean.getCountry","Country is Required");
+        }
+
+        if(userBean.getFirstname() != null){
+            addFieldError("userBean.getFirstname","Firstname is Required");
+        }
+
+        if(userBean.getLastname() != null){
+            addFieldError("userBean.getLastname","Lastname is Required");
+        }
+
+        if(userBean.getNumber() != null){
+            addFieldError("userBean.getNumber","Number is Required");
+        }
+
+        if(userBean.getStreet() != null){
+            addFieldError("userBean.getStreet","Street is Required");
+        }
+
+        if(userBean.getZip() != null){
+            addFieldError("userBean.getZip","Zip is Required");
+        }
 
     }
 }
